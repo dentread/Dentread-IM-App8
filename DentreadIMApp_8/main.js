92feb1ee443dff0c7612785eeb3c8f3b38e11430
bhavik <bhavik@dentread.com>
2023-12-14 13:50:05 -0800

    removing dev tools

--- DentreadIMApp_8/main.js
+++ DentreadIMApp_8/main.js
@@ -57,7 +57,7 @@ function createWindow() {
     // Check for updates and notify
     autoUpdater.checkForUpdatesAndNotify();
   });
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
